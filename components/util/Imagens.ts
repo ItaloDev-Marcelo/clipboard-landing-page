b5498690cf28268f60f15e83b5a9fa9ad1e59e75
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-26 15:07:42 -0300

    feat: criando uma importacao para pegar as images

---
+++ components/util/Imagens.ts
@@ -0,0 +1,36 @@
+import BgHeaderD from '../../assets/images/bg-header-desktop.png'
+import BgHeaderM from '../../assets/images/bg-header-mobile.png'
+import IconBlackList from '../../assets/images/icon-blacklist.svg'
+import IconFacebook from '../../assets/images/icon-facebook.svg'
+import IconInstagram from '../../assets/images/icon-instagram.svg'
+import IconPreview from '../../assets/images/icon-preview.svg'
+import IconText from '../../assets/images/icon-text.svg'
+import IconTwitter from '../../assets/images/icon-twitter.svg'
+import ImageComputer from '../../assets/images/image-computer.png'
+import ImageDevices from '../../assets/images/image-devices.png'
+import LogoGoogle from '../../assets/images/logo-google.png'
+import LogoHp from '../../assets/images/logo-hp.png'
+import LogoIbm from '../../assets/images/logo-ibm.png'
+import LogoMicrosoft from '../../assets/images/logo-microsoft.png'
+import LogoVector from '../../assets/images/logo-vector-graphics.png'
+import Logo from '../../assets/images/logo.svg'
+
+
+export {
+    BgHeaderM,
+    BgHeaderD,
+    IconBlackList,
+    IconFacebook,
+    IconInstagram,
+    IconPreview,
+    IconText,
+    IconTwitter,
+    ImageComputer,
+    ImageDevices,
+    LogoGoogle,
+    LogoHp,
+    LogoIbm,
+    LogoMicrosoft ,
+    LogoVector,
+    Logo
+}
