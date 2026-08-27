6a0bc3c54f21779f4a2b75df96f533eedf7c770b
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-27 14:36:47 -0300

    feat: adicionando o  viweport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata,  Viewport} from "next";
 import { Bai_Jamjuree } from "next/font/google";
 import "./globals.css";
 
@@ -8,6 +8,11 @@ const baiJamjuree = Bai_Jamjuree({
   subsets: ["latin"],
   weight: ["400", "600"],
 });
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
 
 
 export const metadata: Metadata = {
