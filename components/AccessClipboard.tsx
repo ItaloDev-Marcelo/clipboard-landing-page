4b14151358ecbbf0e893154c6d8f7df09cc1a7cd
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-27 13:56:46 -0300

    style: teste de tamanho do estilo

--- components/AccessClipboard.tsx
+++ components/AccessClipboard.tsx
@@ -11,7 +11,7 @@ export const AccessClipboard = () => {
        </div>
 
        <div className='my-7 p-5 xl:p-0'>
-         <Image src={ImageDevices} className='w-full xl:w-75 2xl:w-175 ' alt='devices image'/>
+         <Image src={ImageDevices} className='w-full xl:w-max-200 ' alt='devices image'/>
        </div>
 
     </section>
