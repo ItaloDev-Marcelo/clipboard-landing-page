ed10cecf7d36824839c48a3580cb755bbb5d8b7a
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-08-27 12:10:22 -0300

    style: aplicando correção de estilos nos componentes

--- components/AccessClipboard.tsx
+++ components/AccessClipboard.tsx
@@ -3,15 +3,15 @@ import { ImageDevices } from './util/Imagens'
 
 export const AccessClipboard = () => {
   return (
-    <section className='flex flex-col items-center text-center content-center bg-yellow-500'>
-       <div className='p-5'> 
-        <h2 className='text-[2em] xl:text-[2.7em] text-gray-title text my-5 font-semibold'>Access Clipboard anywhere</h2>
-        <p className='text-gray-p text-[18px] tracking-wide px-5 mb-7 xl:w-190 '>Whether you’re on the go, or at your computer, you can access all your Clipboard 
+    <section className='flex flex-col items-center text-center content-center'>
+       <div className='p-5 xl:p-10 2xl:p-5'> 
+        <h2 className='text-[2em] xl:text-[2.5em] 2xl:text-[2.7em] text-gray-title text my-5 font-semibold'>Access Clipboard anywhere</h2>
+        <p className='text-gray-p text-[18px] xl:text-[15px] 2xl:text-[18px] tracking-wide px-5 mb-7 xl:w-190 '>Whether you’re on the go, or at your computer, you can access all your Clipboard 
   snippets in a few simple clicks.</p>
        </div>
 
        <div className='my-7 p-5 xl:p-0'>
-         <Image src={ImageDevices} alt='devices image'/>
+         <Image src={ImageDevices} className='w-full xl:w-75 2xl:w-175 ' alt='devices image'/>
        </div>
 
     </section>
--- components/Supercharge.tsx
+++ components/Supercharge.tsx
@@ -26,14 +26,14 @@ export const Supercharge = () => {
  const Companies = [LogoGoogle, LogoIbm,LogoMicrosoft,LogoHp,LogoVector]
 
   return (
-    <section className='flex flex-col items-center justify-center content-center bg-red-700'>
-        <div className="text-center px-3 xl:py-4">
+    <section className='flex flex-col items-center justify-center content-center'>
+        <div className="text-center px-3 xl:px-5 xl:py-2 2xl:py-4 ">
             <h2 className="text-[2em]  xl:text-[2.7em] text-gray-title text my-5 font-semibold">Supercharge your workflow</h2>
             <p className='text-gray-p text-[18px] tracking-wide px-5 mb-7 xl:w-190 '>We’ve got the tools to boost your productivity</p>
         </div>
 
-        <div className="flex flex-col items-center justifty-center content-center gap-20 my-4 ">
-        <div className="flex flex-col mt-15 xl:mt-25 gap-5 items-center justify-center xl:flex-row">
+        <div className="flex flex-col items-center justifty-center content-center gap-20 my-4 xl:my-2 2xl:my-4 ">
+        <div className="flex flex-col mt-15 xl:mt-17 2xl:mt-25 gap-5 items-center justify-center xl:flex-row">
             {
                 SuperchargeBlocksData.map((item,index) => (
                     <BlocksSuper key={index} icon={item.icon} title={item.title} text={item.text} />
@@ -41,16 +41,16 @@ export const Supercharge = () => {
             }
         </div>
 
-        <div className="flex flex-col items-center xl:scale-125 2xl:scale-120 mt-15 xl:my-20 xl:flex-row gap-y-20 xl:gap-10">
+        <div className="flex flex-col items-center 2xl:scale-120 mt-15 xl:my-15 2xl:my-20 xl:flex-row gap-y-20 xl:gap-10">
             {
                 Companies.map((item, index)=> (
-                    <Image key={index} src={item} className=" shrink-0 h-auto" alt='company logo' /> 
+                    <Image key={index} src={item} className="shrink-0 h-auto" alt='company logo' /> 
                 ))
             }
         </div>
         </div>
 
-        <section className="gap-5 text-center my-20 p-5 xl:mt-30 ">
+        <section className="gap-5 text-center my-20 p-5 xl:mt-15 2xl:mt-30 ">
            <h2 className="text-[2em] xl:text-[2.7em] text-gray-title text my-5 font-semibold">Clipboard for iOS and Mac OS</h2>
            <p className='text-gray-p text-[18px] tracking-wide px-5 mb-7 xl:w-190 '>Available for free on the App Store. Download for Mac or iOS, sync with iCloud 
   and you’re ready to start adding to your clipboard.</p>
